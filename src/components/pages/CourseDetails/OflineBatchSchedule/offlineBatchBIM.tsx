edc0e02f5580f1ba3e5857593f6da9652785b008
Rakib011110 <rakib088880@gmail.com>
2026-01-29 11:42:38 +0600

    all code added

--- src/components/pages/CourseDetails/OflineBatchSchedule/offlineBatchBIM.tsx
+++ src/components/pages/CourseDetails/OflineBatchSchedule/offlineBatchBIM.tsx
@@ -37,7 +37,7 @@ export default function OfflineBatchSchedule() {
                 {/* Time */}
                 <div className="flex items-center justify-between p-3 bg-black rounded border border-gray-950">
                   <span className="text-white text-lg mt-2 font-medium">সময়:</span>
-                  <span className="text-white font-medium text-lg mt-2">সন্ধ্যা ৮টা - রাত ১০টা</span>
+                  <span className="text-white font-medium text-lg mt-2">সন্ধ্যা ৬টা - রাত ৮টা</span>
                 </div> 
 
               
