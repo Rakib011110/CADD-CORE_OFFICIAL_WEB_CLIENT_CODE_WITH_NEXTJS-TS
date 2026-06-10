7c5f439381e0030c96e3bd4434e4151e7f9e0cf7
Rakib011110 <rakib088880@gmail.com>
2026-06-10 12:57:22 +0600

    Structured Data Markup added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Script from "next/script";
+import "./globals.css";
+import "aos/dist/aos.css";
 import { Providers } from "@/lib/Providers";
 
 const geistSans = Geist({
